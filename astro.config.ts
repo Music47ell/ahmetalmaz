aa956c2a18977c7bae8be7a1a6d7f7dac822b8a2
Ahmet ALMAZ <music47ell@gmail.com>
2025-04-03 13:20:58 +0300

    chore: switch to `libsql/client/web`

--- astro.config.ts
+++ astro.config.ts
@@ -27,6 +27,12 @@ export default defineConfig({
 	},
 
 	vite: {
+		define: {
+			'process.env.DATABASE_URL': JSON.stringify(process.env.DATABASE_URL),
+			'process.env.DATABASE_AUTH_TOKEN': JSON.stringify(
+				process.env.DATABASE_AUTH_TOKEN,
+			),
+		},
 		ssr: {
 			external: ['node:fs/promises'],
 		},
