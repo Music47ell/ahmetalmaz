d058f5219b5f79ad3fa8ede8b919adb93e704ea8
Ahmet ALMAZ <music47ell@gmail.com>
2025-04-13 13:21:38 +0300

    feat: add rehype affiliate links plugin chore: add fallback favicon

--- src/libs/rehype/index.ts
+++ src/libs/rehype/index.ts
@@ -1,2 +1,3 @@
 export { default as rehypeBlockquote } from './rehype-blockquote'
 export { default as rehypeExternalUrlsFavicon } from './rehype-external-urls-favicon'
+export { default as rehypeAffiliateLinks } from './rehype-affiliate-links'
