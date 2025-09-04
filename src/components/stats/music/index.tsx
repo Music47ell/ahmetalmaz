73109e0a0e12b33b66047f0e0d05c133a190ea7a
Ahmet ALMAZ <github.loops6079@ahmetalmaz.com>
2025-09-04 22:24:17 +0300

    chore: update deps ui: removed artists and albums in stats page

--- src/components/stats/music/index.tsx
+++ src/components/stats/music/index.tsx
@@ -1,64 +1,64 @@
-'use client'
+"use client";
 
-import { useEffect, useState } from 'react'
-import RecentTracks from './RecentTracks'
-import TopAlbums from './TopAlbums'
-import TopArtists from './TopArtists'
-import ListenbrainzStats from './ListenBrainzStats'
+import { useEffect, useState } from "react";
+import RecentTracks from "./RecentTracks";
+// import TopAlbums from './TopAlbums'
+// import TopArtists from './TopArtists'
+import ListenbrainzStats from "./ListenBrainzStats";
 
 const MusicStats = () => {
 	const [listenBrainzStats, setListenBrainzStats] = useState<{
-		accountAge: number
-		artistsCount: number
-		tracksCount: number
-		listensCount: number
-		albumsCount: number
+		accountAge: number;
+		artistsCount: number;
+		tracksCount: number;
+		listensCount: number;
+		albumsCount: number;
 	}>({
 		accountAge: 0,
 		artistsCount: 0,
 		tracksCount: 0,
 		listensCount: 0,
 		albumsCount: 0,
-	})
+	});
 	const [recentTracks, setRecentTracks] = useState<
 		{
-			artist: string
-			image?: string
-			title: string
-			preview?: string
+			artist: string;
+			image?: string;
+			title: string;
+			preview?: string;
 		}[]
-	>([])
-	const [topAlbums, setTopAlbums] = useState<
-		{
-			image?: string
-			title: string
-		}[]
-	>([])
-	const [topArtists, setTopArtists] = useState<
-		{
-			name: string
-			image?: string
-		}[]
-	>([])
+	>([]);
+	// const [topAlbums, setTopAlbums] = useState<
+	// 	{
+	// 		image?: string
+	// 		title: string
+	// 	}[]
+	// >([])
+	// const [topArtists, setTopArtists] = useState<
+	// 	{
+	// 		name: string
+	// 		image?: string
+	// 	}[]
+	// >([])
 
 	useEffect(() => {
 		const fetchListenbrainzStats = async () => {
 			try {
-				const response = await fetch('/api/listenbrainz')
+				const response = await fetch("/api/listenbrainz");
 				if (response.ok) {
-					const data = await response.json()
-					setListenBrainzStats(data.stats)
-					setRecentTracks(data.recentTracks)
-					setTopAlbums(data.topAlbums)
-					setTopArtists(data.topArtists)
+					const data = await response.json();
+					setListenBrainzStats(data.stats);
+					setRecentTracks(data.recentTracks);
+					// setTopAlbums(data.topAlbums)
+					// setTopArtists(data.topArtists)
 				}
 			} catch (error) {
-				console.error('Error fetching Listenbrainz stats:', error)
+				console.error("Error fetching Listenbrainz stats:", error);
 			}
-		}
+		};
 
-		fetchListenbrainzStats()
-	}, [])
+		fetchListenbrainzStats();
+	}, []);
 
 	return (
 		<section className="flex flex-col gap-y-2">
@@ -73,7 +73,7 @@ const MusicStats = () => {
 			</div>
 			<RecentTracks tracks={recentTracks} />
 
-			<div className="border border-red-500 px-4 py-2">
+			{/* <div className="border border-red-500 px-4 py-2">
 				<h2 className="uppercase">Monthly Top Albums</h2>
 			</div>
 			<TopAlbums albums={topAlbums} />
@@ -81,9 +81,9 @@ const MusicStats = () => {
 			<div className="border border-red-500 px-4 py-2">
 				<h2 className="uppercase">Monthly Top Artists</h2>
 			</div>
-			<TopArtists artists={topArtists} />
+			<TopArtists artists={topArtists} /> */}
 		</section>
-	)
-}
+	);
+};
 
-export default MusicStats
+export default MusicStats;
