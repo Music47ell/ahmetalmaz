f80056cdfcc1d0a1467bf661c1a689e2389bb9ce
Ahmet ALMAZ <github.loops6079@ahmetalmaz.com>
2025-11-09 20:17:01 +0300

    remove unused code

--- src/pages/api/listenbrainz/top-artists.ts
+++
@@ -1,9 +0,0 @@
-import { getTopArtists } from '../../.././libs/listenbrainz'
-
-export async function GET() {
-	const topArtists = await getTopArtists()
-
-	return new Response(JSON.stringify(topArtists), {
-		headers: { 'content-type': 'application/json' },
-	})
-}
