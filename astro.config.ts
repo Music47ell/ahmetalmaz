54473c7445563f4e716b50428eaece9eda6658af
Ahmet ALMAZ <github.loops6079@ahmetalmaz.com>
2025-11-12 23:20:50 +0300

    remove syntax highlight

--- astro.config.ts
+++ astro.config.ts
@@ -2,12 +2,13 @@ import { loadEnv } from "vite";
 
 import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@astrojs/react'
 import umami from '@yeskunall/astro-umami';
 
 import siteMetadata from './src/data/siteMetadata'
 
 import cloudflare from '@astrojs/cloudflare';
+
+import react from '@astrojs/react';
 
 const { UMAMI_URL, UMAMI_ID } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
@@ -36,13 +37,13 @@ export default defineConfig({
             external: ['node:fs/promises', 'jsdom'],
         },
         plugins: [tailwindcss()],
-        resolve: {
-            // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-            // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-            alias: import.meta.env.PROD && {
-                'react-dom/server': 'react-dom/server.edge',
-            },
-        },
+        // resolve: {
+        //     // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+        //     // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+        //     alias: import.meta.env.PROD && {
+        //         'react-dom/server': 'react-dom/server.edge',
+        //     },
+        // },
     },
     integrations: [react(), umami({
         // doNotTrack: true,
