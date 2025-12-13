ebe630fc88792d569f13adcb73546d1d8754944a
Ahmet ALMAZ <github.loops6079@ahmetalmaz.com>
2025-12-13 11:47:51 +0300

    fix: mastodon webfinger

--- src/pages/.well-known/webfinger.ts
+++ src/pages/.well-known/webfinger.ts
@@ -3,14 +3,17 @@ import siteMetadata from '../../data/siteMetadata';
 const INSTANCE = siteMetadata.instance;
 const USERNAME = siteMetadata.username;
 
-/**
- * This endpoint adds a webfinger JSON file to your site so that you can use
- * your domain as a reference to your Mastodon account.
- */
 export async function GET() {
   const response = await fetch(
     `https://${INSTANCE}/.well-known/webfinger?resource=acct:${USERNAME}@${INSTANCE}`
   );
+
   const data = await response.json();
-  return { body: JSON.stringify(data, null, 2) };
+
+  return new Response(JSON.stringify(data, null, 2), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/jrd+json',
+    },
+  });
 }
