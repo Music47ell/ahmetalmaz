6eb2c65b74ef587db22901e5dc0a864ac501de2d
Ahmet ALMAZ <5708563+Music47ell@users.noreply.github.com>
2026-03-06 23:54:38 +0300

    feat: moving back to github docker image

--- astro.config.ts
+++ astro.config.ts
@@ -5,51 +5,58 @@ import tailwindcss from '@tailwindcss/vite'
 
 import siteMetadata from './src/data/siteMetadata'
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-    site: siteMetadata.siteUrl,
-    trailingSlash: 'never',
-    output: 'server',
-    server: {
-      host: true,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
+  site: siteMetadata.siteUrl,
+  trailingSlash: 'never',
+  output: 'server',
+
+  server: {
+    host: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  },
+
+  env: {
+      schema: {
+          API_BASE_URL: envField.string({
+              context: "client", access: "public", optional: true
+          }),
+          INSIGHT_TOKEN: envField.string({
+              context: "client", access: "public", optional: true
+          }),
+          BLOG_TOKEN: envField.string({
+              context: "server", access: "secret", optional: true
+          }),
+      }
+  },
+
+  build: {
+      format: 'file',
+  },
+
+  prefetch: {
+      defaultStrategy: 'viewport',
+  },
+
+  vite: {
+      // ssr: {
+      //     external: ['node:fs/promises', 'jsdom'],
+      // },
+      plugins: [tailwindcss()],
+      resolve: {
+          // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
+          // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
+          alias: import.meta.env.PROD && {
+              'react-dom/server': 'react-dom/server.edge',
+          },
       },
-    },
-    env: {
-        schema: {
-            API_BASE_URL: envField.string({
-                context: "client", access: "public", optional: true
-            }),
-            INSIGHT_TOKEN: envField.string({
-                context: "client", access: "public", optional: true
-            }),
-            BLOG_TOKEN: envField.string({
-                context: "server", access: "secret", optional: true
-            }),
-        }
-    },
-    build: {
-        format: 'file',
-    },
-    prefetch: {
-        defaultStrategy: 'viewport',
-    },
-    vite: {
-        // ssr: {
-        //     external: ['node:fs/promises', 'jsdom'],
-        // },
-        plugins: [tailwindcss()],
-        resolve: {
-            // Use react-dom/server.edge instead of react-dom/server.browser for React 19.
-            // Without this, MessageChannel from node:worker_threads needs to be polyfilled.
-            alias: import.meta.env.PROD && {
-                'react-dom/server': 'react-dom/server.edge',
-            },
-        },
-    },
-    integrations: [react()],
-    adapter: cloudflare(),
+  },
+
+  integrations: [react()],
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 })
